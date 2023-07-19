7c940276d7fe366fdf0a825bd2dc15762cbfe958
Codingwal <florian.herzog08@gmail.com>
2023-07-19 18:54:16 +0200

    new file:   .gitattributes 	new file:   .gitignore 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/net/neon/neonsfirstmod/Config.java 	new file:   src/main/java/net/neon/neonsfirstmod/NeonsFirstMod.java 	new file:   src/main/java/net/neon/neonsfirstmod/item/ModItems.java 	new file:   src/main/resources/META-INF/mods.toml 	new file:   src/main/resources/assets/neonsfirstmod/lang/en_us.json 	new file:   src/main/resources/assets/neonsfirstmod/models/item/steel.json 	new file:   src/main/resources/assets/neonsfirstmod/textures/item/raw_sapphire.png 	new file:   src/main/resources/assets/neonsfirstmod/textures/item/steel.png 	new file:   src/main/resources/pack.mcmeta 	# modified:   src/main/java/net/neon/neonsfirstmod/NeonsFirstMod.java 	# modified:   src/main/java/net/neon/neonsfirstmod/item/ModItems.java 	# modified:   src/main/resources/assets/neonsfirstmod/lang/en_us.json 	# deleted:    src/main/resources/assets/neonsfirstmod/textures/item/raw_sapphire.png

---
+++ src/main/java/net/neon/neonsfirstmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.neon.neonsfirstmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.neon.neonsfirstmod.NeonsFirstMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            NeonsFirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> Steel = ITEMS.register("steel", 
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventbus) {
+        ITEMS.register(eventbus);
+    }
+}
